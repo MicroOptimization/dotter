43ca219cd5945d14a854d6d54b70d06171231db2
MicroOptimization <jackiezhen538@gmail.com>
2022-10-07 18:48:29 -0400

    created todo list

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Starting")
+"""
+TO-DO
+-----
+Data:
+-Incrementalization of spreadsheet
+-spreadsheet updating
+--getting current date
+
+Frontend
+-Showing
+-Dot Darkening based on productivity progress
+-Grid display
+
+Very Frontend
+-Make this a usable app somehow. (web app, android, idk)
+"""
