bedb643564e5aef780291759296acfde76c87a6c
MicroOptimization <jackiezhen538@gmail.com>
2022-10-17 15:45:35 -0400

    found some useful looking functions from pandas

--- main.py
+++ main.py
@@ -14,3 +14,31 @@ Frontend
 Very Frontend
 -Make this a usable app somehow. (web app, android, idk)
 """
+from datetime import date
+import pandas
+
+date_today = date.today()
+#We're gonna use this^ to check if there's an activity
+#entry for today or not
+
+#print("Today's date:", date_today)
+
+
+df = pandas.read_excel("data.xlsx")
+
+print(df)
+#print(df.index)
+print(df.loc[0]) #important for later
+
+print("Len: " , len(df)) #important for later [number of rows]
+"""
+df = pandas.read_excel("data.xlsx") 
+#print(type(df))
+print(df.columns)
+print("")
+print(df.values)
+print("")
+print(df)
+print("popping")
+print(df.pop("b1"))
+"""
