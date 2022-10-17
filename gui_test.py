b304bcd7c58a634c56ba85eb34b8d6a2a89bbcfc
MicroOptimization <jackiezhen538@gmail.com>
2022-10-17 16:21:21 -0400

    made my grid boxes alot cleaner

--- gui_test.py
+++ gui_test.py
@@ -6,16 +6,16 @@ def create_grid(event=None):
     c.delete('grid_line') # Will only remove the grid_line
 
     # Creates all vertical lines at intevals of 100
-    for i in range(0, w, 100):
-        c.create_line([(i, 0), (i, h)], tag='grid_line')
+    for i in range(70, w - 70, 20):
+        c.create_line([(i, 30), (i, h - 33)], tag='grid_line')
 
     # Creates all horizontal lines at intevals of 100
-    for i in range(0, h, 100):
-        c.create_line([(0, i), (w, i)], tag='grid_line')
+    for i in range(30, h - 30, 20):
+        c.create_line([(70, i), (w - 73, i)], tag='grid_line')
 
 root = tk.Tk()
 
-c = tk.Canvas(root, height=1000, width=1000, bg='white')
+c = tk.Canvas(root, height=200, width=500, bg='white')
 c.pack(fill=tk.BOTH, expand=True)
 
 c.bind('<Configure>', create_grid)
