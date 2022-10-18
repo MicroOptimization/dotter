7b44f38aadef9e45f05a8af41fd20d7b17866558
MicroOptimization <jackiezhen538@gmail.com>
2022-10-17 20:58:39 -0400

    draws a grid now, all the same color though

--- gui_test.py
+++ gui_test.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import *
 """
 def create_grid(event=None):
     #draws empty grid
@@ -14,14 +15,56 @@ def create_grid(event=None):
     for i in range(30, h - 30, 20):
         c.create_line([(70, i), (w - 73, i)], tag='grid_line')
 """
-def create_grid(event=None):
-    tk.canvas.create_rectangle(100, 110, 100, 110, fill = "red")
-    
+#def create_grid(event=None):
+#    tk.Canvas.create_rectangle(100, 110, 100, 110, fill = "red")
+
+
+class grid(Frame):
+    def __init__(self):
+        super().__init__()
+
+        self.make_grid()
+
+    def make_grid(self):
+        self.master.title("Dotter")
+        self.pack(fill=BOTH, expand=0)
+
+        canvas = Canvas(self)
+        xpo = 0 #x positional offset
+        ypo = 0 #y positional offset
+
+        #grid starting points (for first rectangle)
+        gsp_x1 = 22
+        gsp_x2 = 42
+        gsp_y1 = 22
+        gsp_y2 = 42
+        
+        #canvas.create_rectangle(2, 2, 52, 52, outline="#111111", fill="#fb0")
+        grid_width = 15
+        for j in range(grid_width):
+            xpo = j * 20
+            for i in range(7):
+                ypo = i * 20
+                fill_color = "#fb0"
+                canvas.create_rectangle(gsp_x1 + xpo, gsp_y1 + ypo, gsp_x2 + xpo, gsp_y2 + ypo, outline="#111111", fill=fill_color)
+           
+        
+        #canvas.create_rectangle(2, 50, 52, 100, outline="#111111", fill="#fb0")
+
+        
+        
+        canvas.pack(fill=BOTH, expand=0)
+
 root = tk.Tk()
 
-c = tk.Canvas(root, height=200, width=500, bg='white')
+ex = grid()
+
+c = tk.Canvas(root, height=180, width=500, bg='white')
 c.pack(fill=tk.BOTH, expand=True)
 
-c.bind('<Configure>', create_grid)
+#c.bind('<Configure>', create_grid)
+
 
 root.mainloop()
+
+
